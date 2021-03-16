717f983e5eba4a83c2aac2326d02f296598a86db
Dakota Hernandez <46770478+dhernan881@users.noreply.github.com>
2021-03-16 01:20:23 -0400

    Integrated Rashad Quest into map scene.
    A bug still remains where if you click the back button while at a Menu (i.e. dialog option), the game doesn't register that a block is "active," so it doesn't save anything.
    
    Not sure how to fix besides updating the saved block after every command completes, which would require messing around with the base Command class.

--- Assets/Scripts/SaveVN.cs
+++ Assets/Scripts/SaveVN.cs
@@ -6,8 +6,19 @@ using Fungus;
 
 public class SaveVN : MonoBehaviour
 {
+    [Tooltip("Parent GameObject for all flowcharts in the scene. " +
+        "Any flowchart not a child of this won't be considered for saving.")]
     public GameObject flowchartHolder;
+
+    /// <summary>
+    /// A flowchart that holds data for the save point in the visual novel.
+    /// Other data can be stored there as well.
+    /// Currently must be named "Variables Flowchart," but we could 
+    /// make the field public later if needed.
+    /// </summary>
     Flowchart variablesFlowchart;
+
+    // Shorthand references to the saved info
     string fcSavedFlowchartName { get {
             return variablesFlowchart.GetStringVariable("savedFlowchartName");
         } }
@@ -19,17 +30,14 @@ public class SaveVN : MonoBehaviour
         } }
 
     void Awake() {
-        // get load position
-
+        // Make sure we find the Variables Flowchart before trying to
+        // read from it.
         variablesFlowchart = flowchartHolder.transform.
             Find("Variables Flowchart").GetComponent<Flowchart>();
         if (variablesFlowchart == null) {
             Debug.LogError("Couldn't find variables flowchart");
             return;
         }
-        // fcSavedFlowchartName = variablesFlowchart.GetVariable("savedFlowchartName");
-        // fcSavedBlockName = variablesFlowchart.GetVariable("savedBlockName");
-        // fcSavedCommandIndex = variablesFlowchart.GetVariable("savedCommandIndex");
     }
 
     // Start is called before the first frame update
@@ -39,11 +47,9 @@ public class SaveVN : MonoBehaviour
     }
 
     IEnumerator LoadingSavedData() {
-
         yield return new WaitForEndOfFrame();
         // check position in visual novel
         if (fcSavedFlowchartName != "") {
-        // if (!fcSavedFlowchartName.Equals("")) {
             // data has been saved. load position
             Flowchart savedFC = GameObject.Find(fcSavedFlowchartName).
                 GetComponent<Flowchart>();
@@ -56,11 +62,37 @@ public class SaveVN : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Sets the data stored in Variables Flowchart to the arguments.
+    /// </summary>
+    public void SetSavedData(string savedFlowchartName, string savedBlockName,
+        int savedCommandIndex) {
+        variablesFlowchart.SetStringVariable("savedFlowchartName", savedFlowchartName);
+        variablesFlowchart.SetStringVariable("savedBlockName", savedBlockName);
+        variablesFlowchart.SetIntegerVariable("savedCommandIndex", savedCommandIndex);
+    }
+
+    /// <summary>
+    /// Clear saved data. Useful when changing scenes after finishing quests.
+    /// </summary>
+    public void ClearSavedData() {
+        SetSavedData("", "", -1);
+    }
+
+    /// <summary>
+    /// Saves the visual novel info before loading scene sceneName
+    /// </summary>
+    /// <param name="sceneName">The scene to load.</param>
     public void SaveAndLoadScene(string sceneName) {
         SaveInfo();
         SceneManager.LoadScene(sceneName);
     }
 
+    /// <summary>
+    /// Save the active flowchart, block, and command index to Variables
+    /// Flowchart for retrieval earlier. Currently stores one active block,
+    /// but functionality may be added later to support multiple.
+    /// </summary>
     void SaveInfo() {
         string savedFlowchartName = "";
         string savedBlockName = "";
@@ -81,21 +113,6 @@ public class SaveVN : MonoBehaviour
         if (savedBlockName == "") {
             Debug.Log("No executing blocks.");
         }
-        variablesFlowchart.SetStringVariable("savedFlowchartName", savedFlowchartName);
-        variablesFlowchart.SetStringVariable("savedBlockName", savedBlockName);
-        variablesFlowchart.SetIntegerVariable("savedCommandIndex", savedCommandIndex);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void ReturnToScene() {
-        // save position in visual novel
-
-        // add a fade or something?
-        SceneManager.LoadScene("Basic2DMap");
+        SetSavedData(savedFlowchartName, savedBlockName, savedCommandIndex);
     }
 }
