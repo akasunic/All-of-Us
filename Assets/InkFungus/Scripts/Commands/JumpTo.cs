9f0b91f35a0549c61549e0e4223552a94da5a949
Dakota Hernandez <46770478+dhernan881@users.noreply.github.com>
2021-03-19 20:03:54 -0400

    Working on a sample Ink-Fungus and trying to get save/load started

--- Assets/InkFungus/Scripts/Commands/JumpTo.cs
+++ Assets/InkFungus/Scripts/Commands/JumpTo.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 using Fungus;
 using InkFungus;
 
-[CommandInfo("Ink", "Jump To", "Jump to a specified knot or stitch of the Ink story. Use the dot-separated format: 'knot' or 'knot.stitch'.")]
+[CommandInfo("Ink", "Jump To", "Jump to a specified knot or stitch of the Ink story. " +
+    "Use the dot-separated format: 'knot' or 'knot.stitch'. ")]
 public class JumpTo : InkCommand
 {
-    public string knotOrStitch;
+    public StringData knotOrStitch;
 
     public override string GetSummary()
     {
-        return $"Jump to '{knotOrStitch}' in Ink";
+        return $"Jump to '{knotOrStitch.Value}' in Ink";
     }
 
     public override void OnEnter()
     {
-        Director().JumpTo(knotOrStitch);
+        Director().JumpTo(knotOrStitch.Value);
 
         Continue();
     }
