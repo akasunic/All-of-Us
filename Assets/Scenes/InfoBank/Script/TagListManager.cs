27a5520e3ede906b61be0b693ff56ee28ceffe8c
Connie Ye <constanceye1316@gmail.com>
2021-04-26 02:44:30 -0400

    make the lists with tags show "+" instead of "+ add tag"

--- Assets/Scenes/InfoBank/Script/TagListManager.cs
+++ Assets/Scenes/InfoBank/Script/TagListManager.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using System;
- 
+using TMPro;
+
 // this script tracks the tags for the list of tags that we have in the journal
 // it both adds to the tag list from TagManager and ALSO
 // gets tags from the tag list from TagManager
@@ -13,6 +14,8 @@ public class TagListManager : MonoBehaviour {
 
     public RectTransform heightTransform;
     public RectTransform parentTransform;
+
+    public GameObject addButtonText;
 
     void Start(){
       Canvas.ForceUpdateCanvases(); 
@@ -36,7 +39,6 @@ public class TagListManager : MonoBehaviour {
 
 
     public void UpdateList(){
-      Debug.Log("updating");
       List<string> list = TagManager.GetTags(infoId);
 
       for(int i = 0; i < listOfTags.Count; i++){
@@ -48,6 +50,12 @@ public class TagListManager : MonoBehaviour {
         string val = list[i];
         tagItem.GetComponent<DetailPageManager>().setText(val);
         listOfTags.Add(tagItem);
+      }
+
+      if(list.Count == 0){
+        addButtonText.SetActive(true);
+      } else {
+        addButtonText.SetActive(false);
       }
 
     }
