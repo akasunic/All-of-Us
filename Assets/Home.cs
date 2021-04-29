e6b4f78054cee639b682c807f87149e55b28f006
Gal Fleissig <fleissigal@gmail.com>
2021-04-29 16:22:26 -0400

    Fix bug in info button

--- Assets/Home.cs
+++ Assets/Home.cs
@@ -6,53 +6,65 @@ using System.Reflection;
 using System.Threading;
 public class Home : MonoBehaviour {
 
+    public GameObject infoButton;
+    public GameObject goToSleepButton;
     public GameObject backButton;
     public GameObject yellowContainer;
     public GameObject whiteContainer;
     public GameObject separatingLines;
     public GameObject statTitles;
-    public Text yellowContainerText;
-    public Text TitleText;
-    public Text commEngageVal;
-    public Text questsCompletedVal;
-    public Text journalEntriesVal;
-    public Text customTagsVal;
-    public Text daysUntilquestsCompletedVal;
+    public GameObject yellowContainerText;
+    public GameObject TitleText;
+    public GameObject commEngageVal;
+    public GameObject questsCompletedVal;
+    public GameObject journalEntriesVal;
+    public GameObject customTagsVal;
+    public GameObject daysUntilBlockPartyVal;
 
     private void Start() {
 
-        TitleText.enabled = false;
-        yellowContainerText.enabled = false;
-
-        commEngageVal.enabled = false;
-        questsCompletedVal.enabled = false;
-        journalEntriesVal.enabled = false;
-        customTagsVal.enabled = false;
-        daysUntilquestsCompletedVal.enabled = false;
+        // TitleText.enabled = false;
+        // yellowContainerText.enabled = false;
         
-        commEngageVal.text = GlobalGameInfo.GetEngagement().ToString() + "%";
-        questsCompletedVal.text = (GlobalGameInfo.GetEngagement() / 5).ToString() + "/20";
-        journalEntriesVal.text = GlobalGameInfo.infoList.Count.ToString();
-        customTagsVal.text = TagManager.customTags.Count.ToString();
-        daysUntilquestsCompletedVal.text = GlobalGameInfo.GetRemainDays().ToString();
+        commEngageVal.GetComponent<UnityEngine.UI.Text>().text = GlobalGameInfo.GetEngagement().ToString() + "%";
+        questsCompletedVal.GetComponent<UnityEngine.UI.Text>().text = (GlobalGameInfo.GetEngagement() / 5).ToString() + "/20";
+        journalEntriesVal.GetComponent<UnityEngine.UI.Text>().text = GlobalGameInfo.infoList.Count.ToString();
+        customTagsVal.GetComponent<UnityEngine.UI.Text>().text = TagManager.customTags.Count.ToString();
+        daysUntilBlockPartyVal.GetComponent<UnityEngine.UI.Text>().text = GlobalGameInfo.GetRemainDays().ToString();
 
-        yellowContainer.SetActive(false);
-        whiteContainer.SetActive(false);
-        backButton.SetActive(false);
-        separatingLines.SetActive(false);
-        statTitles.SetActive(false);
+        // commEngageVal.enabled = false;
+        // questsCompletedVal.enabled = false;
+        // journalEntriesVal.enabled = false;
+        // customTagsVal.enabled = false;
+        // daysUntilBlockPartyVal.enabled = false;
+
+        // yellowContainer.SetActive(false);
+        // whiteContainer.SetActive(false);
+        // backButton.SetActive(false);
+        // separatingLines.SetActive(false);
+        // statTitles.SetActive(false);
+        
+        // infoButton.SetActive(true);
+        // goToSleepButton.SetActive(true);
 
     }
     
     public void showInfo() {
-        TitleText.enabled = true;
-        yellowContainerText.enabled = true;
 
-        commEngageVal.enabled = true;
-        questsCompletedVal.enabled = true;
-        journalEntriesVal.enabled = true;
-        customTagsVal.enabled = true;
-        daysUntilquestsCompletedVal.enabled = true;
+        TitleText.SetActive(true);
+        yellowContainerText.SetActive(true);
+
+        // commEngageVal.enabled = true;
+        // questsCompletedVal.enabled = true;
+        // journalEntriesVal.enabled = true;
+        // customTagsVal.enabled = true;
+        // daysUntilBlockPartyVal.enabled = true;
+
+        commEngageVal.SetActive(true);
+        questsCompletedVal.SetActive(true);
+        journalEntriesVal.SetActive(true);
+        customTagsVal.SetActive(true);
+        daysUntilBlockPartyVal.SetActive(true);
 
         yellowContainer.SetActive(true);
         whiteContainer.SetActive(true);
@@ -60,21 +72,52 @@ public class Home : MonoBehaviour {
         separatingLines.SetActive(true);
         statTitles.SetActive(true);
 
+        infoButton.SetActive(false);
+        goToSleepButton.SetActive(false);
     }
 
     public void hideInfo() {
-        this.Start();
+
+        TitleText.SetActive(false);
+        yellowContainerText.SetActive(false);
+
+        // commEngageVal.enabled = false;
+        // questsCompletedVal.enabled = false;
+        // journalEntriesVal.enabled = false;
+        // customTagsVal.enabled = false;
+        // daysUntilBlockPartyVal.enabled = false;
+
+        commEngageVal.SetActive(false);
+        questsCompletedVal.SetActive(false);
+        journalEntriesVal.SetActive(false);
+        customTagsVal.SetActive(false);
+        daysUntilBlockPartyVal.SetActive(false);
+
+        yellowContainer.SetActive(false);
+        whiteContainer.SetActive(false);
+        backButton.SetActive(false);
+        separatingLines.SetActive(false);
+        statTitles.SetActive(false);
+        
+        infoButton.SetActive(true);
+        goToSleepButton.SetActive(true);
     }
 
     public void goToSleep() {
-        // Increase day by one
-        GlobalGameInfo.IncreaseDay();
 
-        // Go back to map
-        SceneManager.LoadScene("Basic2DMap");
+        // Only if PC has finished the 5 quests for the day
+        // if (InkFileManager.instance.CanAdvanceDay()) {
 
-        // Toggle new day (triggers hint bubble to say "Good Morning")
-        GlobalGameInfo.toggleNewDay();
+            // Increase day by one
+            GlobalGameInfo.IncreaseDay();
+
+            // Go back to map
+            SceneManager.LoadScene("Basic2DMap");
+
+            // Toggle new day (triggers hint bubble to say "Good Morning")
+            GlobalGameInfo.toggleNewDay();
+
+        // }
     }
 
 }
