8c7fe53c468fcc37ca652c6ff028afdf8080d755
Gal Fleissig <fleissigal@gmail.com>
2021-09-16 17:13:51 -0400

    Export start button string

--- Assets/Opening.cs
+++ Assets/Opening.cs
@@ -1,9 +1,23 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class Opening : MonoBehaviour
 {
+    public TextMeshProUGUI ButtonText;
+
+
+    // Localization Feature
+    public Lang LangClass = new Lang(false);
+
+
+    void Start()
+    {
+        ButtonText.text = LangClass.getString("start");
+    }
+
 
     public void loadBackstoriesScene() {
 
