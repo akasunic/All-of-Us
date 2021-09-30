967f300e06cca9efc17c6d8342900b9acbd25170
Gal Fleissig <fleissigal@gmail.com>
2021-09-30 16:34:13 -0400

    Editing PCsetup scene

--- Assets/Scripts/PCSetUp.cs
+++ Assets/Scripts/PCSetUp.cs
@@ -14,11 +14,13 @@ public class PCSetUp : MonoBehaviour
     public TextMeshProUGUI PronounsText;
     public TextMeshProUGUI LanguageText;
     public TextMeshProUGUI ContinueText;
+    public TextMeshProUGUI InactiveContinueText;
     public Text pronounsDropDownLabel;
     public Text languageDropDownLabel;
     public Dropdown pronounsDropDown;
     public Dropdown languageDropDown;
     public Button continueButton;
+    public Button inactiveContinueButton;
     private string firstName;
     private int intPronouns;
     private string pronouns;
@@ -31,14 +33,15 @@ public class PCSetUp : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Setting texts from Strings.xml
         TitleText.text = LangClass.getString("pc_setup_title");
         NameText.text = LangClass.getString("name_field");
         PronounsText.text = LangClass.getString("pronouns_field");
         LanguageText.text = LangClass.getString("language_field");
         ContinueText.text = LangClass.getString("continue");
+        InactiveContinueText.text = LangClass.getString("continue");
 
-        continueButton.interactable = false;
-
+        // Setting dropdown lists
         List<string> pronounsDropDownOptions = new List<string> { LangClass.getString("she_her"), LangClass.getString("he_his"), LangClass.getString("they_them")};
         List<string> languageDropDownOptions = new List<string> { LangClass.getString("english"), LangClass.getString("french")};
 
@@ -47,7 +50,7 @@ public class PCSetUp : MonoBehaviour
 
         nameInputField.placeholder.GetComponent<Text>().text = LangClass.getString("first_name");
         pronounsDropDown.captionText.text = LangClass.getString("choose_one");
-        languageDropDown.captionText.text = "aaaaaa";
+        languageDropDown.captionText.text = LangClass.getString("choose_one");
     }
 
     // Update is called once per frame
@@ -59,8 +62,14 @@ public class PCSetUp : MonoBehaviour
 
         if (firstName != null && !firstName.Equals("") && intPronouns != 0 && intLanguage != 0)
         {
-            continueButton.interactable = true;
+            continueButton.enabled = false;
+            inactiveContinueButton.enabled = true;
+        } else {
+            continueButton.enabled = true;
+            inactiveContinueButton.enabled = false;
         }
+
+        Debug.Log(firstName);
     }
 
     public void Submit()
