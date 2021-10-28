9bb651fda3332a91cc1050122e7d0f3361bda3b3
Gal Fleissig <fleissigal@gmail.com>
2021-10-28 15:55:55 -0400

    Edit text of slots according to data and create scroll bar

--- Assets/StartWeek.cs
+++ Assets/StartWeek.cs
@@ -8,61 +8,77 @@ public class StartWeek : MonoBehaviour
 
     static public List<GameObject> savedGamesUI;
     public GameObject prefabSavedGameItem;
-    public string[][] savedGamesInfo = new string[3][];
+    public string[][] savedGamesInfo = new string[6][];
     // Start is called before the first frame update
     void Start()
     {
         savedGamesUI = new List<GameObject>();
 
         // savedGamesInfo = new List<List<string>>();
-        savedGamesInfo[0] = new string[3];
-        savedGamesInfo[1] = new string[3];
-        savedGamesInfo[2] = new string[3];
+        savedGamesInfo[0] = new string[4];
+        savedGamesInfo[1] = new string[4];
+        savedGamesInfo[2] = new string[4];
+        savedGamesInfo[3] = new string[4];
+        savedGamesInfo[4] = new string[4];
+        savedGamesInfo[5] = new string[4];
 
-        string[] list = new string[3];
+        string[] list = new string[4];
         list[0] = "Sara";
         list[1] = "3";
         list[2] = "2";
+        list[3] = "1";
         savedGamesInfo[0] = list;
 
-        list = new string[3];
+        list = new string[4];
         list[0] = "Temmie";
         list[1] = "2";
         list[2] = "4";
-        savedGamesInfo[0] = list;
+        list[3] = "2";
+        savedGamesInfo[1] = list;
         
-        list = new string[3];
+        list = new string[4];
         list[0] = "Bob";
         list[1] = "4";
         list[2] = "2";
-        savedGamesInfo[0] = list;
+        list[3] = "3";
+        savedGamesInfo[2] = list;
+
+        list = new string[4];
+        list[0] = "Sara";
+        list[1] = "3";
+        list[2] = "2";
+        list[3] = "4";
+        savedGamesInfo[3] = list;
+
+        list = new string[4];
+        list[0] = "Temmie";
+        list[1] = "2";
+        list[2] = "4";
+        list[3] = "5";
+        savedGamesInfo[4] = list;
+        
+        list = new string[4];
+        list[0] = "Bob";
+        list[1] = "4";
+        list[2] = "2";
+        list[3] = "6";
+        savedGamesInfo[5] = list;
 
         int y_location = 152;
 
         foreach(string[] savedGame in savedGamesInfo) {
             GameObject savedGameItem = Instantiate(prefabSavedGameItem, new Vector3(0, y_location, 0f), Quaternion.identity);
             if (savedGameItem != null) {
-                savedGameItem.transform.SetParent (GameObject.FindGameObjectWithTag("Canvas").transform, false);
-                for (int i = 0; i < savedGameItem.transform.childCount; ++i) {
-                    Transform currentItem = savedGameItem.transform.GetChild(i);
-                    //Search by tag
-                    if (currentItem.CompareTag("NumberText"))
-                    {
-                        // currentItem.text = "1";
-                        continue;
-                    }
-                    if (currentItem.CompareTag("PlayerText"))
-                    {
-                        // currentItem.text = savedGame[0];
-                        continue;
-                    }
-                    if (currentItem.CompareTag("WeekText"))
-                    {
-                        // currentItem.text = "Week " + savedGame[1] + ", Day " + savedGame[2];
-                        continue;
-                    }
-        
-                }
+                savedGameItem.transform.SetParent (GameObject.FindGameObjectWithTag("Content").transform, false);
+                
+                TextMeshProUGUI playerText = savedGameItem.transform.Find("Player").GetComponent<TextMeshProUGUI>();
+                playerText.text = savedGame[0];
+
+                TextMeshProUGUI weekAndDay = savedGameItem.transform.Find("Week and Day").GetComponent<TextMeshProUGUI>();
+                weekAndDay.text = "Week " + savedGame[1] + ", Day " + savedGame[2];;
+                
+                TextMeshProUGUI numberText = savedGameItem.transform.Find("Number Text").GetComponent<TextMeshProUGUI>();
+                numberText.text = savedGame[3];
             }
             y_location -= 135;
         }
