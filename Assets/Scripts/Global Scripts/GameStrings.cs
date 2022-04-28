b2457676bb9d9ad080ab52f23fb99224e0e59aa9
Gal Fleissig <fleissigal@gmail.com>
2022-04-28 14:53:41 -0400

    Remove Backstories and Ending files

--- Assets/Scripts/Global Scripts/GameStrings.cs
+++ Assets/Scripts/Global Scripts/GameStrings.cs
@@ -9,22 +9,6 @@ public static class GameStrings {
     {
         {"English", new Dictionary<string, string>
             {
-                // Backstories
-                {"backstories_1", "There’s so much to love about Bloomwood! The neighborhood is full of caring, vibrant people and plentiful resources. \n\n As the new Community Manager, your role is to learn what your neighbors need, and help them find answers. Every neighbor has problems you can solve, and information, resources, or skills to contribute."},
-                {"backstories_2", "Explore Bloomwood and learn more about your neighbors. Spend a week with each neighbor, and their needs - and thereby your goals - will become clear."},
-                {"backstories_3", ""},
-
-                // Ending
-                {"ending_1", "I’ve been talking so much about Bloomwood Spirit and getting this block party ready.  Today as we blocked off streets and inflated the giant bounce house, I wondered if all my efforts to help did the job. People have bigger issues. Health, money, family, school...did we make enough time? Share enough resources?"},
-                {"ending_2", "Our Block Party is a connection between the old Bloomwood and the new Bloomwood. The smells of food and the whirring of the cotton candy machine all brought back memories. But could this bring the people?"},
-                {"ending_3", "Just as I began to worry, Mr. Calindas arrived to set up the Healthcare tent. He patted me on the back before he went to instruct volunteer nurses on how to work with the community. “Paz and Jessica are showing up soon! Don’t worry!”"},
-                {"ending_4", "I still worried, but it wasn’t long before Lila waved to me as she and Trisha carried art supplies, mobbed by several children who were as excited to see her outside of class as they were to see her in class. “Yes, I bought the facepaint and temporary tattoos! You just have to let me set up!”"},
-                {"ending_5", "Rashad parked the library van, and Octavia and Charles sprinted towards the cotton candy machine. “Ya’ll don’t eat too much of that,” he called out, knowing full well they would eat too much of it."},
-                {"ending_6", "The trickle of people became a warm inviting crowd of neighbors. Mrs. Lee, all smiles, brought her grandson Eddie, who I haven’t seen in forever. “Do you remember my grandson, Eddie? I’m very proud of him.” Eddie looked just as proud of her."},
-                {"ending_7", "Elisa and her family carried succulents, and a few of her friends from school had boxes filled with extras. “We have the perlite, and the rocks. Mini Bloomwood terrariums, these are gonna be so cute!”"},
-                {"ending_8", "We spent the whole day laughing, playing, and celebrating. Mr. Calindas was right. I didn’t need to worry. As music filled the neighborhood and the bounce house shook, I looked at my neighbors. We needed this. I needed this. We could be here, because we all helped each other. Bloomwood spirit has never been about the buildings, or the grants, or the businesses that rush in."},
-                {"ending_9", "It’s always been the people."},
-                
                 // Phone scene
                 {"journal_updated", "MyJournal Updated"},
                 {"todo_updated", "MyTodo Updated"},
@@ -198,14 +182,14 @@ public static class GameStrings {
                 {"end_of_quest_lee_4_sticker1", ""},
                 {"end_of_quest_lee_4_sticker2", ""},
 
-                {"end_of_quest_calindas_1_sticker1", ""},"},
-                {"end_of_quest_calindas_1_sticker2", ""},"},
-                {"end_of_quest_calindas_2_sticker1", ""},"},
-                {"end_of_quest_calindas_2_sticker2", ""},"},
-                {"end_of_quest_calindas_3_sticker1", ""},"},
-                {"end_of_quest_calindas_3_sticker2", ""},"},
-                {"end_of_quest_calindas_4_sticker1", ""},"},
-                {"end_of_quest_calindas_4_sticker2", ""},"},
+                {"end_of_quest_calindas_1_sticker1", ""},
+                {"end_of_quest_calindas_1_sticker2", ""},
+                {"end_of_quest_calindas_2_sticker1", ""},
+                {"end_of_quest_calindas_2_sticker2", ""},
+                {"end_of_quest_calindas_3_sticker1", ""},
+                {"end_of_quest_calindas_3_sticker2", ""},
+                {"end_of_quest_calindas_4_sticker1", ""},
+                {"end_of_quest_calindas_4_sticker2", ""},
 
                 {"end_of_quest_lila_1_sticker1", ""},
                 {"end_of_quest_lila_1_sticker2", ""},
